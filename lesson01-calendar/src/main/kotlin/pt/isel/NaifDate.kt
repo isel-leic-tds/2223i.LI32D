f5ffd8127175cf76adce377dd98089d326209893
Miguel Gamboa <miguelgamboa@outlook.com>
2022-09-14 18:23:05 +0100

    Skeleton for Homework 1

---
+++ lesson01-calendar/src/main/kotlin/pt/isel/NaifDate.kt
@@ -0,0 +1,12 @@
+package pt.isel
+
+class NaifDate(day: Int, month: Int, year: Int) {
+    fun nextMonth(): Int {
+        TODO("Not yet implemented")
+    }
+
+    fun addDays(days: Int): NaifDate {
+            TODO("Not yet implemented")
+    }
+
+}
