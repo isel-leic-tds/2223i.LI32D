402bc7ff0c6469765aafd7e5e9e437c68aaa5126
Miguel Gamboa <miguelgamboa@outlook.com>
2022-11-18 15:39:07 +0000

    lesson 27 setup

---
+++ lesson28-coroutines/src/main/kotlin/pt/isel/App.kt
@@ -0,0 +1,12 @@
+package pt.isel
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.lang.Thread.sleep
+import kotlin.concurrent.thread
+
+fun main() = runBlocking {
+
+}
